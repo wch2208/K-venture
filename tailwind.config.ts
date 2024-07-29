fa5a8a3fc2e4157ce88d36bec336a7c710ca0ea3
wch2208 <97877328+wch2208@users.noreply.github.com>
2024-07-29 15:22:44 +0900

    [GN-88] chore: tailwind 초기 설정 (#4)
    * chore(GN-88): 테일윈드 초기 설정
    
    * chore(GN-88): 전역 폰트와 블랙 색상 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin'
 
 const config: Config = {
   content: [
@@ -8,13 +9,138 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        'kv-3xl': ['32px', { lineHeight: '42px' }],
+        'kv-2xl': ['24px', { lineHeight: '32px' }],
+        'kv-xl': ['20px', { lineHeight: '32px' }],
+        'kv-2lg': ['18px', { lineHeight: '26px' }],
+        'kv-lg': ['16px', { lineHeight: '26px' }],
+        'kv-md': ['14px', { lineHeight: '24px' }],
+        'kv-sm': ['13px', { lineHeight: '22px' }],
+        'kv-xs': ['12px', { lineHeight: '18px' }],
+      },
+      fontWeight: {
+        'kv-regular': '400',
+        'kv-medium': '500',
+        'kv-semibold': '600',
+        'kv-bold': '700',
+      },
+      colors: {
+        'kv-black': {
+          DEFAULT: '#1B1B1B',
+        },
+        'kv-gray': {
+          100: '#FAFAFA',
+          200: '#EEEEEE',
+          300: '#DDDDDD',
+          400: '#CBC9CF',
+          500: '#ADAEB8',
+          600: '#A4A1AA',
+          700: '#6B7280',
+          800: '#374151',
+          900: '#1F2937',
+        },
+        'kv-primary-blue': {
+          DEFAULT: '#3C54D0',
+          light: '#F1EFFD'
+        },
+        'kv-blue': {
+          DEFAULT: '#0085ff',
+          light: '#2eb4ff',
+          lighter: '#e5f3ff',
+        },
+        'kv-red': {
+          DEFAULT: '#F93347',
+          light: '#FFE4E0',
+        },
+        'kv-green': {
+          DEFAULT: '#00AC07',
+        },
+        'kv-orange': {
+          DEFAULT: '#FF7C1D',
+          light: '#FFF4E8',
+        },
+        'kv-yellow': {
+          DEFAULT: '#FFC23D',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+      screens: {
+        pc: { min: '1200px' },
+        tablet: { min: '768px', max: '1199px' },
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-out',
+        fadeOut: 'fadeOut 0.5s ease-out',
+        spin: 'spin 1s linear infinite',
+        'spin-slow': 'spin 10s linear infinite',
+        float: 'float 3s ease-in-out infinite',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities, theme }) => {
+      const newUtilities = {
+        '.kv-text-3xl': {
+          fontSize: theme('fontSize.kv-3xl[0]'),
+          lineHeight: theme('fontSize.kv-3xl[1].lineHeight'),
+        },
+        '.kv-text-2xl': {
+          fontSize: theme('fontSize.kv-2xl[0]'),
+          lineHeight: theme('fontSize.kv-2xl[1].lineHeight'),
+        },
+        '.kv-text-xl': {
+          fontSize: theme('fontSize.kv-xl[0]'),
+          lineHeight: theme('fontSize.kv-xl[1].lineHeight'),
+        },
+        '.kv-text-2lg': {
+          fontSize: theme('fontSize.kv-2lg[0]'),
+          lineHeight: theme('fontSize.kv-2lg[1].lineHeight'),
+        },
+        '.kv-text-lg': {
+          fontSize: theme('fontSize.kv-lg[0]'),
+          lineHeight: theme('fontSize.kv-lg[1].lineHeight'),
+        },
+        '.kv-text-md': {
+          fontSize: theme('fontSize.kv-md[0]'),
+          lineHeight: theme('fontSize.kv-md[1].lineHeight'),
+        },
+        '.kv-text-sm': {
+          fontSize: theme('fontSize.kv-sm[0]'),
+          lineHeight: theme('fontSize.kv-sm[1].lineHeight'),
+        },
+        '.kv-text-xs': {
+          fontSize: theme('fontSize.kv-xs[0]'),
+          lineHeight: theme('fontSize.kv-xs[1].lineHeight'),
+        },
+      } as Record<string, { fontSize: string; lineHeight: string }>;
+      addUtilities(newUtilities);
+    })
+  ],
 };
 export default config;
