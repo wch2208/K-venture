0501355c0cb8d36e7b8c0fb050c1826b9dc0ef61
wch2208 <wch2208@gmail.com>
2024-07-30 15:11:45 +0000

    [GN-10] feat: 헤더 로그인상태 컴포넌트 제작 (#9)
    * feat(GN-10): 헤더 로그인상태 컴포넌트 제작
    
    * fix(GN-10): 로그인상태 컴포넌트 프롭형식 및 문법 수정
    
    ---------
    
    Co-authored-by: 강은우 <80903685+eunwoo66@users.noreply.github.com>

---
+++ src/components/common/HeaderUserProfile.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+interface HeaderUserProfileProps {
+  profileImageUrl: string;
+  nickname: string;
+}
+
+const HeaderUserProfile: React.FC<HeaderUserProfileProps> = ({
+  profileImageUrl,
+  nickname,
+}) => {
+  const DEFAULT_IMAGE = '/assets/icons/icon-profile.svg';
+  const [imgSrc, setImgSrc] = useState(profileImageUrl || DEFAULT_IMAGE);
+
+  const handleImgError = () => {
+    setImgSrc(DEFAULT_IMAGE);
+  };
+
+  return (
+    <div className="flex items-center">
+      <img
+        src={imgSrc}
+        alt={`${nickname}의 프로필`}
+        className="mr-2.5 h-8 w-8 rounded-full"
+        onError={handleImgError}
+      />
+      <span className="font-medium kv-text-md">{nickname}</span>
+    </div>
+  );
+};
+
+export default HeaderUserProfile;
