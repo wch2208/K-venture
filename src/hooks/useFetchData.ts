ad85dad67f2ff7f59d446a89978c197b9ef326ff
wch2208 <wch2208@gmail.com>
2024-08-02 02:11:49 +0000

    [GN-94] feat: tanstack-query 설정 (#12)
    * chore(GN-94): tanstack query 설치
    
    * feat(GN-94): tanstack query 기본 설정
    
    * feat(GN-94): useFetchData 틀 작성

---
+++ src/hooks/useFetchData.ts
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+import { QueryKey, UseQueryResult } from '@tanstack/react-query';
+
+interface Options {
+  enabled?: boolean;
+  // 원하는 경우 기타 옵션 추가 가능
+}
+
+const useFetchData = <T>(
+  queryKey: QueryKey,
+  getService: () => Promise<{ data: T }>,
+  options: Options,
+): UseQueryResult<T, Error> => {
+  return useQuery<T, Error>({
+    queryKey: queryKey,
+    queryFn: async () => {
+      try {
+        const response = await getService();
+        return response.data;
+      } catch (error) {
+        // 에러 핸들링 가능
+        throw error;
+      }
+    },
+    ...options,
+  });
+};
+
+export default useFetchData;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import '@/styles/globals.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import localFont from 'next/font/local';
+import { useState } from 'react';
 
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
@@ -14,11 +17,29 @@ const pretendard = localFont({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Next.js에서는 페이지를 이동하면 App 컴포넌트부터 새롭게 렌더링되기 때문에
+  // 쿼리 클라이언트가 매번 새롭게 생성되는 것을 막기 위하여 state로 저장
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // 보통 SSR에서는 staleTime을 0 이상으로 해줌으로써
+            // 클라이언트 사이드에서 바로 다시 데이터를 refetch 하는 것을 피한다.
+            // * default 값은 0
+          },
+        },
+      }),
+  );
+
   return (
-    <main className={`${pretendard.variable} font-pretendard`}>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className={`${pretendard.variable} font-pretendard`}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
